bf756ab00cb33c864b616a76f6bde30cdc92f608
mayur <mayurbeno@yahoo.comgit config --global user.name Mayur>
2020-07-01 18:44:41 +0530

    Added imaged and code

--- covidapp/views.py
+++ covidapp/views.py
@@ -9,4 +9,7 @@ import json
 def index(request):
     total_count = requests.get('https://api.kawalcorona.com/')
     countrywise_count = total_count.json()
-    return render(request, 'index.html', {'countrywise_count': countrywise_count})
+    india_response = requests.get('http://covid-19india-api.herokuapp.com/all')
+    # transfor the response to json objects
+    india_count = india_response.json()
+    return render(request, 'index.html', {'countrywise_count': countrywise_count, 'india_count': india_count})
